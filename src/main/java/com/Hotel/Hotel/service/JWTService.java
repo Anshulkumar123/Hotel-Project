07717a95482208491e2742309a3e9631ddbe6916
Anshulkumar123 <168398368+Anshulkumar123@users.noreply.github.com>
2024-07-20 14:37:22 +0530

    Hotel project v1

--- src/main/java/com/Hotel/Hotel/service/JWTService.java
+++ src/main/java/com/Hotel/Hotel/service/JWTService.java
@@ -1,9 +1,13 @@
 package com.Hotel.Hotel.service;
 
+import com.Hotel.Hotel.entity.PropertyUser;
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JWTService {
@@ -15,10 +19,18 @@ public class JWTService {
     private int expiryTime;
     private Algorithm algorithm;
 
+    private final static String USER_NAME="username";
+
     @PostConstruct
     public void postConstruct(){
-        System.out.println(algorithmKey);
-        System.out.println(issuer);
-        System.out.println(expiryTime);
+        algorithm = algorithm.HMAC256(algorithmKey);
+    }
+
+    public String generateToken(PropertyUser propertyUser){
+        return JWT.create()
+                .withClaim(USER_NAME,propertyUser.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+expiryTime))
+                .withIssuer(issuer)
+                .sign(algorithm);
     }
 }
